68c9bc55758c710ff719bc4d885298203e22c6bd
Muzam Ali <muzamdty@gmail.com>
2023-02-02 01:56:05 +0500

    remove code

--- vEcom/Models/Brands.cs
+++ vEcom/Models/Brands.cs
@@ -15,7 +15,5 @@ namespace vEcom.Models
         public string BrandName { get; set; }
         public string BrandLogo { get; set; }
 
-        public int testing { get; set; }
-        public string dkdfkdf { get; set; }
     }
 }
